2008731f426bf160523ad780dc70388e71792f48
Andrew Yaroshevych <ayaroshlvua@gmail.com>
2022-08-19 21:15:16 +0300

    Added function for building `translation_language_selector`

--- web/static/scripts/language_selector.js
+++ web/static/scripts/language_selector.js
@@ -1,6 +1,4 @@
 $(function() {
-    console.log("Language selector loaded")
-
     let language_selector = $('#language_selector');
     let request = new XMLHttpRequest();
     request.open("GET", "../../static/assets/speech_supported_languages.json", false);
@@ -10,5 +8,18 @@ $(function() {
     let toAppend = '';
     $.each(data,function(language, code){
        language_selector.append('<option value='+code+'>'+language+'</option>');
+    });
+})
+
+$(function() {
+    let translation_language_selector = $('#translate_language_selector');
+    let request = new XMLHttpRequest();
+    request.open("GET", "../../static/assets/translate_supported_languages.json", false);
+    request.send(null)
+    let data = JSON.parse(request.responseText);
+
+    let toAppend = '';
+    $.each(data,function(language, code){
+       translation_language_selector.append('<option value='+code+'>'+language+'</option>');
     });
 })
